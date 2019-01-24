93f953212b3dde9f87659d7724ca30b2d459cce4
Neo <hu.yu.huei@gmail.com>
2019-01-25 01:53:30 +0800

    Subject [Feature] : 加入VLC lib Message : 1. 將app固定Landscape 2. 加入VLC Lib

--- app/src/main/java/com/ufistudio/hotelmediabox/pages/home/HomeFragment.kt
+++ app/src/main/java/com/ufistudio/hotelmediabox/pages/home/HomeFragment.kt
@@ -1,16 +1,19 @@
 package com.ufistudio.hotelmediabox.pages.home
 
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.MediaController
 import com.ufistudio.hotelmediabox.R
 import com.ufistudio.hotelmediabox.pages.TemplateViewModel
 import com.ufistudio.hotelmediabox.pages.base.InteractionView
 import com.ufistudio.hotelmediabox.pages.base.OnPageInteractionListener
 import kotlinx.android.synthetic.main.fragment_home.*
+import java.net.URI
 
 class HomeFragment : InteractionView<OnPageInteractionListener.Primary>() {
 
@@ -33,5 +36,7 @@ class HomeFragment : InteractionView<OnPageInteractionListener.Primary>() {
         list_functions.layoutManager = LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false)
         list_functions.adapter = mAdapter
 
+//        videoView.setMediaController(MediaController(context))
+//        videoView.setVideoURI(Uri.parse("http://194.88.107.101:6204/Arnaud/123/9019"))
     }
 }
