7dff6e924f40c20204e2708545eaeb91a6648a9e
Neo <hu.yu.huei@gmail.com>
2019-01-27 23:22:42 +0800

    Subject [Feature] : Message : 加入ExoPlayer lib

--- app/src/main/java/com/ufistudio/hotelmediabox/pages/home/HomeFragment.kt
+++ app/src/main/java/com/ufistudio/hotelmediabox/pages/home/HomeFragment.kt
@@ -1,21 +1,37 @@
 package com.ufistudio.hotelmediabox.pages.home
 
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.exoplayer2.DefaultLoadControl
+import com.google.android.exoplayer2.DefaultRenderersFactory
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.upstream.DefaultDataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 import com.ufistudio.hotelmediabox.R
 import com.ufistudio.hotelmediabox.pages.TemplateViewModel
 import com.ufistudio.hotelmediabox.pages.base.InteractionView
 import com.ufistudio.hotelmediabox.pages.base.OnPageInteractionListener
 import kotlinx.android.synthetic.main.fragment_home.*
+import javax.sql.DataSource
 
 class HomeFragment : InteractionView<OnPageInteractionListener.Primary>() {
 
     private lateinit var mViewModel: TemplateViewModel
     private var mAdapter = FunctionsAdapter()
+
+    private lateinit var mPlayer: SimpleExoPlayer
 
     companion object {
         fun newInstance(): HomeFragment = HomeFragment()
@@ -27,11 +43,30 @@ class HomeFragment : InteractionView<OnPageInteractionListener.Primary>() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
     override fun onStart() {
         super.onStart()
 
-        list_functions.layoutManager = LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false)
+        list_functions.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
         list_functions.adapter = mAdapter
+
+        initPlayer()
+    }
+
+
+    private fun initPlayer() {
+        mPlayer = ExoPlayerFactory.newSimpleInstance(
+            context,
+            DefaultRenderersFactory(context),
+            DefaultTrackSelector(),
+            DefaultLoadControl()
+        )
+
+        videoView.player =mPlayer
+
 
     }
 }
