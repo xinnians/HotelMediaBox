e6ab354850a0420486bfc033b9167f11fbd96129
ian Lin <plstyxs@gmail.com>
2019-05-09 14:25:10 +0800

    [feature] 設定預設字體及粗體選擇

--- app/src/main/java/com/ufistudio/hotelmediabox/pages/base/BaseActivity.kt
+++ app/src/main/java/com/ufistudio/hotelmediabox/pages/base/BaseActivity.kt
@@ -1,7 +1,12 @@
 package com.ufistudio.hotelmediabox.pages.base
 
+import android.content.Context
+import android.graphics.Typeface
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.ufistudio.hotelmediabox.pages.base.BaseActivity.TypeFaceProvider.Montserrat_Light
+import com.ufistudio.hotelmediabox.pages.base.BaseActivity.TypeFaceProvider.Montserrat_Medium
+import java.util.*
 
 abstract class BaseActivity : AppCompatActivity(), OnPageInteractionListener.Base {
 
@@ -17,21 +22,21 @@ abstract class BaseActivity : AppCompatActivity(), OnPageInteractionListener.Bas
     }
 
     /**
-     * 將系統預設的Serif字型設成PING_FANG_REGULAR 以及Monospace替換成PING_FANG_MEDIUM
-     * 而預設使用字型經由style設成Serif，所以若需要使用PING_FANG_MEDIUM須在該元件上進行設定android:typeface="monospace"
+     * 將系統預設的Serif字型設成Montserrat_Light 以及Monospace替換成Montserrat_Medium
+     * 而預設使用字型經由style設成Serif，所以若需要使用Montserrat_Medium須在該元件上進行設定android:typeface="monospace"
      * */
     private fun initFontStyle() {
-//        try {
-//            val fieldSerif = Typeface::class.java.getDeclaredField("SERIF")
-//            fieldSerif.isAccessible = true
-//            fieldSerif.set(null, TypeFaceProvider.getTypeFace(this, PING_FANG_REGULAR))
-//
-//            val fieldMonospace = Typeface::class.java.getDeclaredField("MONOSPACE")
-//            fieldMonospace.isAccessible = true
-//            fieldMonospace.set(null, TypeFaceProvider.getTypeFace(this, PING_FANG_MEDIUM))
-//        } catch (e: Exception) {
-//            e.printStackTrace()
-//        }
+        try {
+            val fieldSerif = Typeface::class.java.getDeclaredField("SERIF")
+            fieldSerif.isAccessible = true
+            fieldSerif.set(null, TypeFaceProvider.getTypeFace(this, Montserrat_Light))
+
+            val fieldMonospace = Typeface::class.java.getDeclaredField("MONOSPACE")
+            fieldMonospace.isAccessible = true
+            fieldMonospace.set(null, TypeFaceProvider.getTypeFace(this, Montserrat_Medium))
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
     override fun pressBack() {
@@ -64,25 +69,25 @@ abstract class BaseActivity : AppCompatActivity(), OnPageInteractionListener.Bas
 //        mFullScreenMessage?.isCancelable = false
     }
 
-//    object TypeFaceProvider{
-//
-//        const val PING_FANG_MEDIUM = "PingFangTC-Medium.ttf"
-//        const val PING_FANG_REGULAR = "PingFangTC-Regular.ttf"
-//
-//        private val typeFaces = Hashtable<String, Typeface>(3)
-//
-//        fun getTypeFace(context: Context, fontName: String): Typeface? {
-//            var typeface = typeFaces[fontName]
-//
-//            if(typeface == null) run {
-//                var fontPath: String = "fonts/$fontName"
-//                typeface = Typeface.createFromAsset(context.assets, fontPath)
-//
-//                typeFaces.put(fontName, typeface)
-//            }
-//
-//            return typeface
-//        }
-//
-//    }
+    object TypeFaceProvider {
+
+        const val Montserrat_Light = "Montserrat-Light.ttf"
+        const val Montserrat_Medium = "Montserrat-Medium.ttf"
+
+        private val typeFaces = Hashtable<String, Typeface>(3)
+
+        fun getTypeFace(context: Context, fontName: String): Typeface? {
+            var typeface = typeFaces[fontName]
+
+            if (typeface == null) run {
+                var fontPath: String = "fonts/$fontName"
+                typeface = Typeface.createFromAsset(context.assets, fontPath)
+
+                typeFaces.put(fontName, typeface)
+            }
+
+            return typeface
+        }
+
+    }
 }
